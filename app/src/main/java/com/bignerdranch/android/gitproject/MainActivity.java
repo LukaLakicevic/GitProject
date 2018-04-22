83dbbf239b6f1ab23c8c2e93e95f006aef04e935
BADI\Laki <lakiluka12@gmail.com>
2018-04-22 17:41:13 +0200

    Added onClick listener for HitMe Button

--- app/src/main/java/com/bignerdranch/android/gitproject/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/gitproject/MainActivity.java
@@ -2,12 +2,27 @@ package com.bignerdranch.android.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String a = "First Toast Ever";
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.HitButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, a, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
